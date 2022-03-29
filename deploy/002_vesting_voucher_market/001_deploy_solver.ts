e278953495dcd6b8954e4659420934d38c5dc571
Triv <cryptotriv@gmail.com>
2022-03-29 14:23:43 +0800

    Add missing deployment tags to Solver

--- deploy/002_vesting_voucher_market/001_deploy_solver.ts
+++ deploy/002_vesting_voucher_market/001_deploy_solver.ts
@@ -20,4 +20,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 export default func;
 func.id = 'deploy_solver'; // id required to prevent reexecution
-func.tags = ['FullDeployment','FullVestingVoucherDeployment', 'Solver'];
+func.tags = ['FullDeployment','FullVestingVoucherDeployment', "FullIVODeployment", 'Solver'];
